4faa8da3e4143900e0db14068a79867e17a7fec9
adventin <Andrew-Esin@yandex.ru>
2023-03-15 22:12:34 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+
+function New(props) {
+  return (
+    <div className="wrap-item wrap-item-new">
+      <span className="label">New!</span>
+      {props.children}
+    </div>
+  )
+};
+
+function Popular(props) {
+  return (
+    <div className="wrap-item wrap-item-popular">
+      <span className="label">Popular!</span>
+      {props.children}
+    </div>
+  )
+};
+
+function Article(props) {
+  return (
+    <div className="item item-article">
+      <h3><a href="#">{props.title}</a></h3>
+      <p className="views">Прочтений: {props.views}</p>
+    </div>
+  )
+};
+
+function Video(props) {
+  return (
+    <div className="item item-video">
+      <iframe src={props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+      <p className="views">Просмотров: {props.views}</p>
+    </div>
+  )
+};
+
+const prepareComponent = (WrappedComponent) => (props) => {
+
+  const { views } = props;
+
+  switch (true) {
+    case (views < 100):
+      return <New {...props}><WrappedComponent {...props} /></New>;
+    case (views > 1000):
+      return <Popular {...props}><WrappedComponent {...props} /></Popular>;
+    default:
+      return <WrappedComponent {...props} />;
+  }
+
+};
+
+const VideoPrepared = prepareComponent(Video);
+const ArticlePrepared = prepareComponent(Article);
+
+function List(props) {
+  return props.list.map((item, index) => {
+    switch (item.type) {
+      case 'video':
+        return (
+          <VideoPrepared key={index} {...item} />
+        );
+
+      case 'article':
+        return (
+          <ArticlePrepared key={index} {...item} />
+        );
+    }
+  });
+};
+
+export default function App() {
+  const [list, setList] = useState([
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 50
+    },
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 12
+    },
+    {
+      type: 'article',
+      title: 'Невероятные события в неизвестном поселке...',
+      views: 175
+    },
+    {
+      type: 'article',
+      title: 'Секретные данные были раскрыты!',
+      views: 1532
+    },
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 4253
+    },
+    {
+      type: 'article',
+      title: 'Кот Бегемот обладает невероятной...',
+      views: 12,
+    },
+  ]);
+
+  return (
+    <List list={list} />
+  );
+}
